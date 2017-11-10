c1e614b6d8f000744032ffd10f1ee10ed6a7bda8
shivayl <joao95neves@gmail.com>
2017-11-10 19:00:01 +0000

    Update main.js
      . Linked the API (JSON) to the index.html weather showcaser;
    
      . Added functionality to change the temperature from Celsius to Fahrenheit;
    
      . Added an icon for each sky condition (from the API);
    
      . Added comments.

--- Weather_App/main.js
+++ Weather_App/main.js
@@ -1,27 +1,53 @@
+$('#scale-btn').addClass('hide')
 $(document).ready(function () {
   let usrLatitude
   let usrLongitude
+  let celsius
   // User Geolocation:
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
       usrLatitude = position.coords.latitude
       usrLongitude = position.coords.longitude
-      console.log(usrLatitude)
-      console.log(usrLongitude)
 
       // API Call:
+      // Test URL: https://api.openweathermap.org/data/2.5/weather?lat=38.7018707&lon=-9.228257700000002&appid=2be16b4f9b9e0645e776071c8092b02e
       let APILink = 'https://api.openweathermap.org/data/2.5/weather?lat=' + usrLatitude + '&lon=' + usrLongitude + '&appid=2be16b4f9b9e0645e776071c8092b02e'
       $.ajax({
         url: APILink,
         success: function (json) {
-          console.log(json.main.temp)
-          console.log(json.weather[0].main)
-          console.log(json.name)
-          console.log(json.sys.country)
+          $('#sky').html(json.weather[0].main)
+          $('#sky-icon').attr('src', 'http://openweathermap.org/img/w/' + json.weather[0].icon +'.png')
+          $('#sky-icon').attr('alt', json.weather[0].main)
+          $('#location').html(json.name + ', ' + json.sys.country)
+          // The temperature comes in Kelvin Scale (.C = .K - 273.15)
+          celsius = (json.main.temp - 273.15).toFixed(2)
+          $('#temp').html(celsius + ' &#x2103;')
+          // Show the Temperature Scale Button:
+          $('#scale-btn').removeClass('hide')
         }
       })
     })
   } else {
     console.log('Geolocation Not Available')
   }
+  // Change Degree Scale Toggle Button (Celsius <-> Fahrenheit):
+  function toCelsius (fahrenheit) {
+    return ((fahrenheit - 32) / 1.8).toFixed(2)
+  }
+
+  let fahrenheit
+  function toFahrenheit (celsius) {
+    fahrenheit = (celsius * 1.8 + 32).toFixed(2)
+    return fahrenheit
+  }
+
+  $('#change-scale').change(function () {
+    if (this.checked) {
+      $('#temp').html(toFahrenheit(celsius) + ' &#x2109;')
+      console.log('Checked')
+    } else {
+      $('#temp').html(toCelsius(fahrenheit) + ' &#x2103;')
+      console.log('Not Checked')
+    }
+  })
 })
