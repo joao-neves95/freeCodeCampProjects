280fae580a25bf3fa13ea4ffe176777bfa40b1e6
shivayl <joao95neves@gmail.com>
2017-11-16 22:59:12 +0000

    Update main.js
      . The ```MediaWiki API``` is now linked and fully functional;
    
      . The (Wiki Cards) wikipedia content search results are now injected on the page with ```JavaScript```;
    
      . Prepared the cards clickable link to wikipedia page functiuonality (propbably open an ```iFrame```)
    
      . Refactored some animation parts in order to facilitate any changings in the future;
    
      . Added comments.

--- Wikipedia_Viewer_App/main.js
+++ Wikipedia_Viewer_App/main.js
@@ -1,91 +1,89 @@
-/* API Documentation: https://www.mediawiki.org/wiki/API:Main_page & https://www.mediawiki.org/wiki/API:Query & https://www.mediawiki.org/wiki/API:Search & https://www.mediawiki.org/wiki/API:Search_and_discovery
+/* API Documentation: https://www.mediawiki.org/wiki/API:Main_page & https://www.mediawiki.org/wiki/API:Search & https://www.mediawiki.org/wiki/API:Search_and_discovery
       Query Parameters I can use:
       prop=extracts [https://www.mediawiki.org/w/api.php?action=help&modules=query%2Bextracts]
       prop=pageviews [https://www.mediawiki.org/w/api.php?action=help&modules=query%2Bpageviews]
       list=search [https://www.mediawiki.org/w/api.php?action=help&modules=query%2Bsearch]
-
-      Learn about redirects.
-
-      How MediaWiki search works (read later):
-
-        It searches all pages on the wiki with some restrictions.
-
-        The article content is searched in its raw (wikitext) form - i.e.,
-          it searches the text that appears in the edit box when you click "edit",
-          not the rendered page. This means that content coming from an included
-          template will not be picked up, but the target of piped links will be.
-
-        The search is not case-sensitive, so 'MediaWiki', 'mediawiki' and 'MEDIAWIKI' all give the same result.
-
-        The search functionality can be considered to operate on whole words, separated by spaces or other
-          punctuation marks. So if your search term includes the word 'book', the results will not include
-          pages that only have the word 'books' or 'booklet'. And if your search term includes the term 'inter',
-          the results will not include pages that only have the word 'international', but they may include pages
-          that have the term 'inter-national'.
-
-        The results will only include pages that contain all the words in your search.
-        You can search for a phrase using double quotes. A phrase can be considered to consist of whole words
-          (case-insensitive), so the phrase 'Prime Minister' will not be found by a search for "ime Min",
-          but it will be found by a search for "pRIME mINISTER".
-
 */
-$('#search-form, #about').hide()
+$('#search-form, #about-page').hide()
 $(document).ready(function () {
+
+  function showSearchForm () {
+    $('#search-form').slideDown()
+    $('#search-btn-container').addClass('active')
+  }
+
+  function hideSearchForm () {
+    $('#search-form').slideUp()
+    $('#search-btn-container').removeClass('active')
+  }
+
+  function showAboutPage () {
+    $('#about-page').slideDown()
+    $('#about-btn-container').addClass('active')
+  }
+  function hideAboutPage () {
+    $('#about-page').hide()
+    $('#about-btn-container').removeClass('active')
+  }
+
   // SEARCH BUTTON:
   $('#search-btn').click(function () {
     if ($('#search-btn-container').hasClass('active')) {
-      $('#search-form').slideUp()
-      $('#search-btn-container').removeClass('active')
+      hideSearchForm()
     } else {
-      $('#about').hide()
-      $('#about-btn-container').removeClass('active')
-      $('#search-form').slideDown()
-      $('#search-btn-container').addClass('active')
+      hideAboutPage()
       $('main').show()
+      showSearchForm()
     }
   })
-  // Search Input (ENTER keypress) + Wiki API:
+
+  // On ENTER keypress: Search Input + Wiki API:
   $('#search-input').on('keypress', function (e) {
     if (e.keyCode === 13) {
+      // Clean the Wiki Cards Holder inner HTML:
+      document.getElementById('wiki-cards-holder').innerHTML = ''
       let searchInput = document.getElementById('search-input').value
-      console.log(searchInput)
       // API Call:
-      let wikiTitle
-      let wikiContent
-      // Test Link: https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
-      let APIlink = 'https://en.wikipedia.org/w/api.php' + '&format=json'
-      $.ajax( {
-        url: APIlink,
-        data: queryData,
+        // Test Link: https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=hello&format=json
+      let APIlink = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&srsearch=' + searchInput + '&format=json'
+      $.ajax(APIlink, {
         dataType: 'json',
-        type: 'POST',
-        headers: { 'Api-User-Agent': 'Wikipedia-Viewer-freeCodeCamp/0.0' },
-        success: function (json) {
+        data: {
+          origin: '*'
         },
-        error: function () {
-          console.log('ERROR MESSAGE')
+        type: 'GET',
+        success: function (json) {
+          // Add the Wiki Cards, each one with their content, to the Wiki Cards Holder inner HTML:
+          for (let i = 0; i < json.query.search.length; i++) {
+            document.getElementById('wiki-cards-holder').innerHTML += '<a target="_blank" href="https://en.wikipedia.org/wiki/Hello">' +
+                                                                        '<div class="col m12">' +
+                                                                          '<article class="card horizontal z-depth-2 hoverable magictime vanishIn">' +
+                                                                            '<div class="card-content">' +
+                                                                              '<div class="card-title">' +
+                                                                                '<h4 id="wiki-title">' + json.query.search[i].title + '</h4>' +
+                                                                              '</div>' +
+                                                                              '<p class="wiki-content">' +
+                                                                                json.query.search[i].snippet +
+                                                                              '</p>' +
+                                                                            '</div>' +
+                                                                          '</article>' +
+                                                                        '</div>' +
+                                                                      '</a>'
+          }
         }
-    } );
-
-      let wikiCard = {
-        title: wikiTitle,
-        content: wikiContent
-      }
+      })
     }
   })
 
   // ABOUT BUTTON:
   $('#about-btn').click(function () {
     if ($('#about-btn-container').hasClass('active')) {
-      $('#about').hide()
-      $('#about-btn-container').removeClass('active')
+      hideAboutPage()
       $('main').show()
     } else {
       $('main').hide()
-      $('#search-form').slideUp()
-      $('#search-btn-container').removeClass('active')
-      $('#about').slideDown()
-      $('#about-btn-container').addClass('active')
+      hideSearchForm()
+      showAboutPage()
     }
   })
 })
