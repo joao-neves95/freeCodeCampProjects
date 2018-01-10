eda7740e02b68fdc658e43eda53f8b97e90193fe
shivayl <joao95neves@gmail.com>
2018-01-10 20:17:39 +0000

    Create server.js

---
+++ WhoAmI_Microservice/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routes = require('./routes/index.js')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use(express.static('public'))
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+// API:
+app.use(express.json())
+app.use("/api", routes)
+
+// Listen in Glitch.com PORT:
+var listener = app.listen(process.env.PORT, function () {
+  console.log('The server is listening on port ' + listener.address().port);
+})
