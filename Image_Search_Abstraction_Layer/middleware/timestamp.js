f9bff30b6fa44a3c00238603765ef717fb9c4bf2
shivayl <joao95neves@gmail.com>
2018-01-22 21:23:21 +0000

    Create timestamp.js

---
+++ Image_Search_Abstraction_Layer/middleware/timestamp.js
@@ -0,0 +1,8 @@
+'use strict'
+const newTimestamp = () => {
+    const date = new Date().toISOString().substr(0, 10)
+    const time = new Date().toISOString().substr(11, 12)
+    return date + ' ' + time
+}
+
+module.exports = newTimestamp
